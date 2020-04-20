c116d9fddc793086b00956cc5f0c1323535bf658
Ashish Shakya <ashishakya@outlook.com>
2020-04-20 12:21:38 +0545

    Middleware

--- server.js
+++ server.js
@@ -3,15 +3,22 @@
 const express = require('express');
 const app = express();
 
+let helloMiddleware = (req, res, next)=>{
+    req.hello = 'Hello from the other side';
+    next();
+}
+
+app.use('/dashboard', helloMiddleware);
+
 app.set('port', process.env.PORT || 8001)
 
-
 app.get('/', (req, res, next) => {
+    console.log(req.hello);
     res.send('<h1>Hello from express</h1>');
 })
 
 app.get('/dashboard', (req, res, next)=>{
-    res.send('<h1>Hey this is dashboard!!</h1>')
+    res.send('<h1>Hey this is dashboard!!' + req.hello + '</h1>')
 })
 
 app.listen(app.get('port'), () => {
