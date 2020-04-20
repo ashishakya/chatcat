926691d8da4898c03523def47bc8b58c4b064c91
Ashish Shakya <ashishakya@outlook.com>
2020-04-20 12:57:36 +0545

    - [x] Use ejs for view rendering - [x] Separate the route

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
 
-app.set('port', process.env.PORT || 8001)
+app.set('port', process.env.PORT || 8001);
+// setting templating engine
 app.set('view engine', 'ejs');
+// middleware function to serve static file
+app.use(express.static('public'));
+// setting routes;
+app.use('/', chatCat.router);
 
-app.get('/', (req, res, next) => {
-    res.render('login');
-})
 
 app.listen(app.get('port'), () => {
     console.log('Chat cat running in port: ', app.get('port'));
