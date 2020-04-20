4f040e46a2e6471980642f9a40b17c3a01bb27b0
Ashish Shakya <ashishakya@outlook.com>
2020-04-20 12:36:33 +0545

    Render html with ejs templating

--- server.js
+++ server.js
@@ -3,22 +3,11 @@
 const express = require('express');
 const app = express();
 
-let helloMiddleware = (req, res, next)=>{
-    req.hello = 'Hello from the other side';
-    next();
-}
-
-app.use('/dashboard', helloMiddleware);
-
 app.set('port', process.env.PORT || 8001)
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-    console.log(req.hello);
-    res.send('<h1>Hello from express</h1>');
-})
-
-app.get('/dashboard', (req, res, next)=>{
-    res.send('<h1>Hey this is dashboard!!' + req.hello + '</h1>')
+    res.render('login');
 })
 
 app.listen(app.get('port'), () => {
