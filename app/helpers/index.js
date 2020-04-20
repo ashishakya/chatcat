3a602a2a1bbc0d9b0c5bc662b9a0bdfc21535885
Ashish Shakya <ashishakya@outlook.com>
2020-04-20 14:13:50 +0545

    Separate helper function for registering routes

---
+++ app/helpers/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const router = require('express').Router();
+
+
+// iterate through the routes object and mount the routes
+// _for denoting as the private function
+let _registerRoutes = (routes, method) => {
+    for (let key in routes) {
+        if (typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
+            _registerRoutes(routes[key], key);
+        } else {
+            // register the routes for resolve
+            if (method === 'get') {
+                router.get(key, routes[key]);
+            } else if (method === 'post') {
+                router.post(key, routes[key]);
+            }
+        }
+    }
+}
+
+let route = routes => {
+    _registerRoutes(routes);
+    return router;
+}
+
+module.exports = {
+    route
+}
+
---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const router = require('express').Router();
+const h = require('../helpers');
+
+module.exports = () => {
+    let routes = {
+        'get': {
+            '/': (req, res, next) => {
+                res.render('login');
+            },
+            '/rooms': (req, res, next) => {
+                res.render('rooms');
+            },
+            '/chat': (req, res, next) => {
+                res.render('chatroom');
+            }
+        },
+        'post': {}
+    }
+
+    return h.route(routes);
+}
