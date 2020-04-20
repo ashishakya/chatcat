c76f9f50cc13d3a03dc7c3630eb540117118fb15
Ashish Shakya <ashishakya@outlook.com>
2020-04-20 10:53:35 +0545

    - [x] Create instance of express - [x] use express.get for route - [x] use express.set and express.get for port no and other env file

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 'use strict'
 
-console.log('welcome to chatchat');
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 8001)
+
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>');
+})
+
+app.get('/dashboard', (req, res, next)=>{
+    res.send('<h1>Hey this is dashboard!!</h1>')
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Chat cat running in port: ', app.get('port'));
+})
